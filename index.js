2caf604ffa5b5f7ca5ae2ee5347bc8b1ecc980be
prethi06 <prethis9506@gmail.com>
2026-01-14 11:14:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+        //smooth scrolling
+        document.querySelectorAll('a[hreaf^="#"]').forEach
+        (anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+        //navbar scroll effect
+        window.addEventListener('scroll',()=> {
+            const navbar = document.querySelector('.navbar');
+            window.scrollY>50?
+                navbar.style.backgroundColor = 'rgba(10,10,10.0.98)':
+                navbar.style.backgroundColor = 'rgba(10,10,10.0.95)':
+        });
